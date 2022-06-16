3f4028cfabfa71d2305badfbebbb64ebda0f4691
Jamaluddin Rumi <jamal.rumi@icloud.com>
2022-06-16 18:36:46 +0700

    import tailwind.css in the last order

--- app.vue
+++ app.vue
@@ -9,18 +9,34 @@
   </NuxtLayout>
 </template>
 
-<style>
-@import "@/assets/css/app.css";
-@import "@/assets/css/inter.css";
-</style>
-
 <style lang="scss">
 @import "@inkline/inkline/css/variables";
 @import "@inkline/inkline/css/mixins";
+@import "@/assets/css/app.css";
+@import "@/assets/css/inter.css";
+@import "@/assets/css/tailwind.css";
 
 :root {
-    --color--primary--h: 202deg;
-    --color--primary--s: 89%;
-    --color--primary--l: 43%;
+  --color--primary--h: 202deg;
+  --color--primary--s: 89%;
+  --color--primary--l: 43%;
+}
+
+.fa-ul {
+  list-style-type: none;
+  margin-left: 2.5em;
+  padding-left: 0;
+}
+
+.fa-ul>li, .fa-ul>a {
+    position: relative;
+}
+
+.fa-li {
+  left: -2em;
+  position: absolute;
+  text-align: center;
+  width: 2em;
+  line-height: inherit;
 }
 </style>
