1dbcbf98baf117e6006539e2c355887712c133a3
Jamaluddin Rumi <jamal.rumi@icloud.com>
2022-06-16 14:24:35 +0700

    use layouts & pages

--- app.vue
+++ app.vue
@@ -1,12 +1,7 @@
 <template>
-  <div class="">
-    <div class="min-h-screen grid place-content-center">
-      <h1 class="text-center text-2xl"><span class="font-bold">Jamal</span> <span
-          class=" opacity-75 text-sm align-text-top">Blog</span>
-      </h1>
-      <h2 class="text-center">about NuxtJS, Marketing, UI/UX</h2>
-    </div>
-  </div>
+  <NuxtLayout name="default">
+    <NuxtPage></NuxtPage>
+  </NuxtLayout>
 </template>
 
 <style>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="min-h-screen grid place-content-center">
+        <h1 class="text-center text-2xl"><span class="font-bold">Jamal</span><span
+                class=" opacity-75 text-sm align-text-top">Blog</span>
+        </h1>
+        <h2 class="text-center">about NuxtJS, Marketing, UI/UX</h2>
+    </div>
+</template>
