8c7509bb3cd2abaac16714b5f74715fe058dba99
Jamaluddin Rumi <jamal.rumi@icloud.com>
2022-06-16 14:24:54 +0700

    add meta title to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,8 @@
 <template>
+
+    <Head>
+        <Title>Marketing Practitioner - Jamaluddin Rumi</Title>
+    </Head>
     <div class="min-h-screen grid place-content-center">
         <h1 class="text-center text-2xl"><span class="font-bold">Jamal</span><span
                 class=" opacity-75 text-sm align-text-top">Blog</span>
