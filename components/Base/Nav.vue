24455f2daeedb8a7f5e625fa0345606572f48c0a
Jamaluddin Rumi <jamal.rumi@icloud.com>
2022-06-18 00:47:00 +0700

    set margin bottom to 0 on navbar nav

--- components/Base/Nav.vue
+++ components/Base/Nav.vue
@@ -7,7 +7,7 @@
             </div>
         </i-navbar-brand>
         <i-navbar-collapsible class="_justify-content:center">
-            <i-nav class="fa-ul mb-4 md:mb-0">
+            <i-nav class="fa-ul mb-4">
                 <i-nav-item class="pl-0 mr-8" to="/">
                     <span class="fa-li">
                         <IconNuxt class="align-text-bottom inline-block opacity-75"></IconNuxt>
