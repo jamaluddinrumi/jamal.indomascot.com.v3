06b9a9f3e8ca58f59ca6c34ce2bdb0a82d34beb6
Jamaluddin Rumi <jamal.rumi@icloud.com>
2022-07-28 22:36:23 +0700

    update home page content

--- pages/index.vue
+++ pages/index.vue
@@ -3,15 +3,32 @@
     <Head>
       <Title>{{ $t("title") }} - Jamaluddin Rumi</Title>
     </Head>
-    <h1 class="text-center">Halaman Depan</h1>
-    <h2 class="text-center text-sm">
-      {{ $t("title") }}
-    </h2>
+    <div class="flex justify-center items-start">
+      <h1 class="text-center inline-flex">Coming</h1>
+      <IconSoon
+        class="icon align-text-bottom inline-flex w-11 h-11 mt-[calc(1rem_*_2.5)] ml-2"
+      ></IconSoon>
+    </div>
+    <p class="container text-center text-sm">
+      My name is Jamaluddin Rumi and Welcome to my personal website. I'm a Web
+      Developer from Indonesia and work as Owner at
+      <NuxtLink href="https://www.indomascot.com">INDOMASCOT</NuxtLink>. I'm
+      also bootstrapping a startup called TanahkuLaku, a marketplace especially for
+      trading land in Indonesia.
+    </p>
   </div>
 </template>
 
 <script setup>
+import IconSoon from "~icons/noto/soon-arrow";
+
 definePageMeta({
   name: "halaman",
 });
 </script>
+
+<style lang="scss">
+.icon {
+  line-height: var(--heading--line-height);
+}
+</style>
