cac35cc2736cbbb393d96564c2ac60fc2570215c
Jamaluddin Rumi <jamal.rumi@icloud.com>
2022-10-02 21:39:20 +0700

    set importan to false in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  important: true,
+  important: false,
   content: [],
   theme: {
     extend: {
