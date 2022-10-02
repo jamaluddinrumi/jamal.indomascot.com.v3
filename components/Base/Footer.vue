58d18a7a47309997a23021601730c1d21de13c21
Jamaluddin Rumi <jamal.rumi@icloud.com>
2022-10-02 21:47:04 +0700

    styling footer links

--- components/Base/Footer.vue
+++ components/Base/Footer.vue
@@ -2,18 +2,26 @@
   <i-layout-footer class="">
     <div class="flex justify-between">
       <i-nav>
-        <i-nav-item class="text-xs opacity-75" to="/privacy">{{
-          $t("footer.privacy.title")
-        }}</i-nav-item>
-        <i-nav-item class="text-xs opacity-75" to="/about">{{
-          $t("footer.about.title")
-        }}</i-nav-item>
-        <i-nav-item class="text-xs opacity-75" to="/contact">{{
-          $t("footer.contact.title")
-        }}</i-nav-item>
-        <i-nav-item class="text-xs opacity-75" to="/credits">{{
-          $t("footer.credits.title")
-        }}</i-nav-item>
+        <i-nav-item to="/privacy"
+          ><span class="text-xs opacity-75 font-medium">{{
+            $t("footer.privacy.title")
+          }}</span></i-nav-item
+        >
+        <i-nav-item to="/about"
+          ><span class="text-xs opacity-75 font-medium">{{
+            $t("footer.about.title")
+          }}</span></i-nav-item
+        >
+        <i-nav-item to="/contact"
+          ><span class="text-xs opacity-75 font-medium">{{
+            $t("footer.contact.title")
+          }}</span></i-nav-item
+        >
+        <i-nav-item to="/credits"
+          ><span class="text-xs opacity-75 font-medium">{{
+            $t("footer.credits.title")
+          }}</span></i-nav-item
+        >
       </i-nav>
       <i-nav></i-nav>
     </div>
